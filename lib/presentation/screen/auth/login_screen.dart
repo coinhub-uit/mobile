381bb2318f2dd0cbc0200bdf71a0bbc32813ffc2
Hen Huynh <Huynhchihen2005@gmail.com>
2025-02-25 15:12:24 +0700

    feat(login): add Login UI

--- lib/presentation/screen/auth/login_screen.dart
+++ lib/presentation/screen/auth/login_screen.dart
@@ -1,10 +1,245 @@
-import "package:flutter/widgets.dart";
+import "package:coinhub/core/services/auth.dart";
+import "package:flutter/gestures.dart";
+import "package:flutter/material.dart";
+// TODO: add flutter_svg package
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 160, 16, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const WelcomeText(
+                title: "Welcome to CoinHub",
+                text: "Please enter your Email\nor Password to login.",
+              ),
+              const SignInForm(),
+              const SizedBox(height: 16),
+              Center(
+                child: Text(
+                  "Or",
+                  style: TextStyle(
+                    color: Color(0xFF010F07).withValues(alpha: 0.7),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16 * 1.5),
+
+              Center(
+                child: Text.rich(
+                  TextSpan(
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                    text: "Don’t have account? ",
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "Create new account.",
+                        style: const TextStyle(
+                          color: Color(0xFF4B399F),
+                          fontWeight: FontWeight.bold,
+                        ),
+                        recognizer:
+                            TapGestureRecognizer()
+                              ..onTap = () {
+                                // Navigate to Sign Up Screen
+                              },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+
+              // Google
+              //TODO: add Google logo
+              SocalButton(
+                press: () {
+                  AuthService.signInWithGoogle();
+                },
+                text: "Connect with Google",
+                color: const Color(0xFF4285F4),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomeText extends StatelessWidget {
+  final String title, text;
+
+  const WelcomeText({super.key, required this.title, required this.text});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 16),
+        Text(
+          title,
+          style: Theme.of(
+            context,
+          ).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.w600),
+        ),
+        const SizedBox(height: 16 / 2),
+        Text(text, style: TextStyle(color: Color(0xFF868686))),
+        const SizedBox(height: 24),
+      ],
+    );
+  }
+}
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({super.key});
+
+  @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            onSaved: (value) {},
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "Email Address",
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xFFF3F2F2)),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xFFF3F2F2)),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+
+          // Password Field
+          TextFormField(
+            obscureText: _obscureText,
+            onSaved: (value) {},
+            decoration: InputDecoration(
+              hintText: "Password",
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xFFF3F2F2)),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xFFF3F2F2)),
+              ),
+              suffixIcon: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+                child:
+                    _obscureText
+                        ? const Icon(
+                          Icons.visibility_off,
+                          color: Color(0xFF868686),
+                        )
+                        : const Icon(
+                          Icons.visibility,
+                          color: Color(0xFF868686),
+                        ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+
+          // Forget Password
+          Align(
+            alignment: Alignment.centerRight,
+            child: GestureDetector(
+              onTap: () {},
+              child: Text(
+                "Forgot Your Password?",
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF4B399F),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+
+          // Sign In Button
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFF4B399F),
+              foregroundColor: Colors.white,
+              minimumSize: const Size(double.infinity, 40),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            child: const Text("Sign in"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SocalButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final GestureTapCallback press;
+
+  const SocalButton({
+    super.key,
+    required this.color,
+    required this.press,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const padding = EdgeInsets.symmetric(horizontal: 16, vertical: 8);
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: padding,
+          backgroundColor: color,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+        ),
+        onPressed: press,
+        child: Text(
+          text.toUpperCase(),
+          style: Theme.of(context).textTheme.bodySmall!.copyWith(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
   }
 }
