ed20f8f7108492e2d13d49806047d9a4368f208c
Khong <hankhongg@gmail.com>
2025-03-14 20:21:19 +0700

    feat: implement depositScreen

---
+++ lib/presentation/components/mini_source_card.dart
@@ -0,0 +1,79 @@
+import "package:flutter/material.dart";
+import "package:intl/intl.dart";
+
+class MiniSourceCard extends StatelessWidget {
+  final String sourceId;
+  final int moneyInit;
+  final IconData icon;
+  final Color color;
+
+  const MiniSourceCard({
+    required this.moneyInit,
+    required this.sourceId,
+    required this.icon,
+    required this.color,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    String vndFormat(int amount) {
+      // format money to VND
+      final formatter = new NumberFormat("#,###", "vi_VN");
+      return "${formatter.format(amount)}đ";
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width / 2 - 52,
+        height: 50,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Row(
+                children: [
+                  Icon(
+                    icon,
+                    size: 30,
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                  SizedBox(width: 8),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          sourceId,
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onSurface,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          vndFormat(moneyInit),
+                          style: TextStyle(
+                            color: Theme.of(
+                              context,
+                            ).colorScheme.onSurface.withOpacity(0.8),
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/screen/transaction/deposit_screen.dart
+++ lib/presentation/screen/transaction/deposit_screen.dart
@@ -1,3 +1,4 @@
+import "package:coinhub/presentation/components/mini_source_card.dart";
 import "package:flutter/material.dart";
 
 class DepositScreen extends StatefulWidget {
@@ -7,11 +8,100 @@ class DepositScreen extends StatefulWidget {
 }
 
 class _DepositScreenState extends State<DepositScreen> {
+  late int selectedIndex;
+
+  @override
+  void initState() {
+    selectedIndex = 0;
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Deposit")),
-      body: Center(child: Text("Deposit Screen")),
+      appBar: AppBar(
+        title: Text("Deposit"),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 16,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Deposit into:",
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    SizedBox(
+                      height: 50,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 3,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedIndex = index;
+                              });
+                            },
+                            child: MiniSourceCard(
+                              icon: Icons.account_balance_wallet,
+                              moneyInit: 10000000,
+                              sourceId: "098573821",
+                              color:
+                                  selectedIndex == index
+                                      ? Theme.of(
+                                        context,
+                                      ).colorScheme.onSecondary.withOpacity(0.8)
+                                      : Theme.of(context).colorScheme.secondary,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(decoration: InputDecoration(labelText: "Amount")),
+                    SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: 60, // Adjust height
+          child: FloatingActionButton(
+            onPressed: () {
+              print("Button Pressed");
+            },
+            child: Text(
+              "Deposit",
+              style: Theme.of(
+                context,
+              ).textTheme.displayMedium!.copyWith(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
